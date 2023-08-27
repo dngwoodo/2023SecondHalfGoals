bddbb133a1e2992e488c910c3e408044aa2c6bb7
Molly <dngwoodo@inflab.com>
2023-08-27 15:08:33 +0900

    docs: nest 실행시 flow 주석 정리

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/**
+ * @description
+ * app 실행 => 
+ * NestFactory 실행 => 
+ * AppModule 실행 => 
+ * AppController 실행 => 
+ * (app.controller.ts)라우트 코드 실행 => 
+ * 서비스 코드 실행(app.service.ts)
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
